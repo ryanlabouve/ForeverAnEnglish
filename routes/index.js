0c475cccceef267d0cca19b9b67c076e6b21a283
Ryan LaBouve <imryanlabouve@gmail.com>
2016-02-06 20:15:06 -0600

    update title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Forever An English' });
 });
 
 module.exports = router;
